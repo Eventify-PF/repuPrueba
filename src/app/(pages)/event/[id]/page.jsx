6dbbb54591018774fe39cd3b9ead23c01ae2afe5
iyas89 <iyasasaad@gmail.com>
2023-11-08 01:08:44 -0300

    detail

--- src/app/(pages)/event/[id]/page.jsx
+++ src/app/(pages)/event/[id]/page.jsx
@@ -1,16 +1,26 @@
 import EventDetailClient from "./EventDetailClient";
 import axios from "axios";
 
-const loadEvent = async(id) =>{
-  const res = await axios.get(`https://server-eventifypro.onrender.com/events/${id}`);
-  const data = await res.json();
-  return data
-}
-export default async function EventPage ({params}){
-  const detailEvent = await loadEvent(params.id);
-  
-  return (
-      <EventDetailClient detailEvent={detailEvent}/>
-  );
+const loadEvent = async (id) => {
+  try {
+    const res = await axios.get(`https://server-eventifypro.onrender.com/events/${id}`);
+    const data = res.data; // Utiliza res.data en lugar de await res.json() para axios
+    return data;
+  } catch (error) {
+    console.error("Error al cargar el evento:", error);
+    throw error; // Lanza el error para que se propague
+  }
+};
+
+export default async function EventPage({ params }) {
+  try {
+    const detailEvent = await loadEvent(params.id);
+
+    return <EventDetailClient detailEvent={detailEvent} />;
+  } catch (error) {
+    // Puedes manejar el error aquí, redirigir o mostrar un mensaje de error en caso de que falle la carga.
+    console.error("Error en la página de eventos:", error);
+    return <div>Error al cargar el evento. Por favor, inténtalo de nuevo más tarde.</div>;
+  }
 }
 
