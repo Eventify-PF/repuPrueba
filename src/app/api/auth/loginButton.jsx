5a6bde076ce1a77af1a61d69a2ae69834443e40d
iyas89 <iyasasaad@gmail.com>
2023-11-07 23:15:59 -0300

    auth0

---
+++ src/app/api/auth/loginButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+//const Login = () => {
+//  return <a href="/api/auth/login">Login</a>;
+//};
+
+//export default Login;
+
+import { useRouter } from 'next/navigation';
+
+const Logout = () => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    
+    router.push('/api/auth/login');
+  };
+
+  return (
+    <button className="rounde mr-3 hidden bg-blue-700 py-1.5 px-6 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none   md:mr-0 md:inline-block rounded-lg" onClick={handleLogout}>Login</button>
+  );
+};
+
+export default Logout;
