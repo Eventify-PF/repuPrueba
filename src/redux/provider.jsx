cb7dcfa2c992412cdad132546fb6343c6cd0a980
iyas89 <iyasasaad@gmail.com>
2023-11-07 23:37:35 -0300

    event page

--- src/redux/provider.jsx
+++ src/redux/provider.jsx
@@ -3,9 +3,9 @@ import store from "./store";
 import { Provider } from "react-redux";
 import axios from "axios";
 
-// axios.defaults.baseURL= 'http://localhost:3001';
+axios.defaults.baseURL= 'http://localhost:3001';
 
-axios.defaults.baseURL= 'https://server-eventifypro.onrender.com';
+// axios.defaults.baseURL= 'https://server-eventifypro.onrender.com';
 
 export function Providers({ children }) {
   return <Provider store={store}>{children}</Provider>;
