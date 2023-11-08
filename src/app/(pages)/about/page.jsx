e05389a56066226676e8796793a9b5a62987ca97
iyas89 <iyasasaad@gmail.com>
2023-11-08 00:26:03 -0300

    pages

--- src/app/(pages)/about/page.jsx
+++ src/app/(pages)/about/page.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import Carrusel from '@/components/Carrusel/Carrusel';
 const AboutPage = () => {
   return (
     <div>
       <h1>About</h1>
+      <Carrusel />
     </div>
   );
 }
--- src/app/(pages)/event/[id]/page.jsx
+++ src/app/(pages)/event/[id]/page.jsx
@@ -1,6 +1,8 @@
 import EventDetailClient from "./EventDetailClient";
+import axios from "axios";
+
 const loadEvent = async(id) =>{
-  const res = await fetch(`http://localhost:3001/events/${id}`);
+  const res = await axios.get(`https://server-eventifypro.onrender.com/events/${id}`);
   const data = await res.json();
   return data
 }
